245679197bc6130a516a8e07792d0fcddde9d0c1
Pranav Srivastava <pranav.srivastava.1291@gmail.com>
2026-02-13 19:15:12 +0530

    feat:Pranav/WT - added vertical progress bar in create application in Water tanker, enhanced workflow timeline UI and UX, fixed the alignments of form throughout the WT module, Changed UI of module cards and showed the appropriate details on the card from API.

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/wt/src/components/inbox/InboxLink.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/wt/src/components/inbox/InboxLink.js
@@ -36,7 +36,7 @@ const InboxLinks = ({ parentRoute, businessService }) => {
   return (
     // <Card style={{ paddingRight: 0, marginTop: 0 }} className="employeeCard filter inboxLinks">
     <Card className="employeeCard filter inboxLinks">
-      <div className="complaint-links-container">
+      <div className="complaint-links-container" style={{ padding: "10px" }}>
         {GetLogo()}
         {/* <div style={{ marginLeft: "unset", paddingLeft: "0px" }} className="body"> */}
         <div className="body">
