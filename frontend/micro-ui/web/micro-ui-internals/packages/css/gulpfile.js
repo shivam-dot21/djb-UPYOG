bd1f662b97d3049c8a2b74a813ea4085f2c6a503
Sparrow-Satyam <satyam_gupta@sparrowsoftech.net>
2026-02-25 11:37:48 +0530

    Reverted gulpfile.js

--- frontend/micro-ui/web/micro-ui-internals/packages/css/gulpfile.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/css/gulpfile.js
@@ -1,14 +1,55 @@
+// const { series, src, dest, watch } = require("gulp");
+// const clean = require("gulp-clean");
+// const postcss = require("gulp-postcss");
+// const cleanCSS = require("gulp-clean-css");
+// const rename = require("gulp-rename");
+
+// let output = "./dist";
+// let nodeModulesDest = "../../../node_modules/@djb25/digit-ui-css/dist"; // 🔥 copies to web/node_modules
+
+// function cleanStyles() {
+//   return src(`${output}/*.css`, { read: false, allowEmpty: true }).pipe(clean());
+// }
+
+// function styles() {
+//   const plugins = [
+//     require("postcss-import"),
+//     require("tailwindcss"),
+//     require("autoprefixer"),
+//     require("postcss-preset-env")({
+//       stage: 2,
+//       autoprefixer: { cascade: false },
+//       features: { "custom-properties": true },
+//     }),
+//   ];
+
+//   return src("src/index.scss").pipe(postcss(plugins)).pipe(rename("index.css")).pipe(dest(output)).pipe(dest(nodeModulesDest)); // 🔥 copy to node_modules
+// }
+
+// function minify() {
+//   return src(`${output}/index.css`).pipe(cleanCSS()).pipe(rename("index.min.css")).pipe(dest(output)).pipe(dest(nodeModulesDest)); // 🔥 copy minified also
+// }
+
+// function watcher() {
+//   console.log("👀 Watching SCSS files...");
+//   watch("src/**/*.scss", series(styles, minify));
+// }
+
+// exports.build = series(cleanStyles, styles, minify);
+// exports.watch = watcher;
+// exports.default = series(styles, minify);
+
 const { series, src, dest, watch } = require("gulp");
 const clean = require("gulp-clean");
 const postcss = require("gulp-postcss");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
+const livereload = require("gulp-livereload");
 
 let output = "./dist";
-let nodeModulesDest = "../../../node_modules/@djb25/digit-ui-css/dist"; // 🔥 copies to web/node_modules
 
 function cleanStyles() {
-  return src(`${output}/*.css`, { read: false, allowEmpty: true }).pipe(clean());
+  return src(`${output}/*.css`, { read: false }).pipe(clean());
 }
 
 function styles() {
@@ -21,20 +62,24 @@ function styles() {
       autoprefixer: { cascade: false },
       features: { "custom-properties": true },
     }),
+    require("cssnano"),
   ];
 
-  return src("src/index.scss").pipe(postcss(plugins)).pipe(rename("index.css")).pipe(dest(output)).pipe(dest(nodeModulesDest)); // 🔥 copy to node_modules
+  return src("src/index.scss")
+    .pipe(postcss(plugins)) // Tailwind+Apply+Theme → CSS
+    .pipe(rename("index.css"))
+    .pipe(dest(output));
 }
 
 function minify() {
-  return src(`${output}/index.css`).pipe(cleanCSS()).pipe(rename("index.min.css")).pipe(dest(output)).pipe(dest(nodeModulesDest)); // 🔥 copy minified also
+  return src(`${output}/index.css`).pipe(cleanCSS()).pipe(rename("index.min.css")).pipe(dest(output));
 }
 
-function watcher() {
-  console.log("👀 Watching SCSS files...");
-  watch("src/**/*.scss", series(styles, minify));
+function livereloadStyles() {
+  livereload.listen();
+  watch("src/**/*.scss", series(styles));
 }
 
 exports.build = series(cleanStyles, styles, minify);
-exports.watch = watcher;
 exports.default = series(styles, minify);
+exports.watch = livereloadStyles;
